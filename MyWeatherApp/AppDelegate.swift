6eca2abbd8a724847ceb4d7592b5d459cb268e9b
Vladislav <vladislav.dudevich@letmecode.org>
2020-06-03 15:25:51 +0300

    create WeatherApp project

---
+++ MyWeatherApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MyWeatherApp
+//
+//  Created by vladislav on 03.06.2020.
+//  Copyright © 2020 vladislav. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ MyWeatherApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MyWeatherApp
+//
+//  Created by vladislav on 03.06.2020.
+//  Copyright © 2020 vladislav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
